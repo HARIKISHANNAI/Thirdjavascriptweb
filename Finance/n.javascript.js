5c371df2ac38fd33e61f0f8c40ffd897646fc09f
Harikishan <143779570+HARIKISHANNAI@users.noreply.github.com>
2023-12-19 00:12:22 +0530

    Add files via upload

---
+++ Finance/n.javascript.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $(".mbu").click(function(){
+        $(".mbul").slideToggle(1000);
+    });
+});
+$(document).ready(function(){
+    $("#df").click(function(){
+    $(".mff").fadeIn(function(){
+
+    });
+    });
+});
+$(document).ready(function(){
+    $("#ds").click(function(){
+    $(".mfs").fadeIn(1000);
+    });
+});
+$(document).ready(function(){
+    $("#dt").click(function(){
+    $(".mft").fadeIn(1000);
+    });
+});
+$(document).ready(function(){
+    $("#dff").click(function(){
+    $(".mfff").fadeIn(1000);
+    });
+});
+$(document).ready(function(){
+    $("#dfff").click(function(){
+    $(".mffff").fadeIn(1000);
+    });
+});
+$(document).ready(function(){
+    $("#dss").click(function(){
+    $(".mfss").fadeIn(1000);
+    });
+});
